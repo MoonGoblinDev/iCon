02b69aa843cef864aa3fb99da28c9a4d71686c1b
MoonGoblin <bregas.sat1@gmail.com>
2025-05-11 19:50:59 +0800

    Initial Commit

---
+++ iCon/iConApp.swift
@@ -0,0 +1,17 @@
+//
+//  iConApp.swift
+//  iCon
+//
+//  Created by Bregas Satria Wicaksono on 11/05/25.
+//
+
+import SwiftUI
+
+@main
+struct iConApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
